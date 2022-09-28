95fbe48059da7fa49017d352d19c5e8441d02404
Shaan Vieru <hshaan@ymail.com>
2022-09-28 13:41:29 +0000

    Add solution to frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,10 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for el in items:
+        try:
+            frequencies[str(el)] += 1
+        except KeyError:
+            frequencies[str(el)] = 1
+
     return frequencies
